c8416d92d8ab527790d2aa73e6bc200e3f776b4c
skyisfalling <skyisfalling@hotmail.com>
2015-12-02 02:01:07 -0800

    changes on submit button and CSS appending li

--- yellow-fan-pig/js/yellow-fan-pig.js
+++ yellow-fan-pig/js/yellow-fan-pig.js
@@ -164,7 +164,7 @@ function createComment(comment) {
   var li = document.createElement("li");
 
   // update the value for the total
-  totalContent = comment['name'] + " " + comment['city'] + " " + comment['email'] + " " + comment['message']
+  totalContent = "Name:" + comment['name'] + " " + "City:" + comment['city'] + " " + "Email:" + comment['email'] + " " + "Comment:" + comment['message']
 
   // set the text content for both the new list item and the total
   li.textContent = totalContent 
