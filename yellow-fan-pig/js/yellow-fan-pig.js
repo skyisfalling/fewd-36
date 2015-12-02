23e6b3f424b39e73b08f39abe349a1f04b9159a6
skyisfalling <skyisfalling@hotmail.com>
2015-12-02 01:42:27 -0800

    no skrollr

--- yellow-fan-pig/js/yellow-fan-pig.js
+++ yellow-fan-pig/js/yellow-fan-pig.js
@@ -177,17 +177,7 @@ function createComment(comment) {
 // Utility functions
 // ------------------------------------------------
 
-//Skrollr
 
-( function( $ ) {
-    // Init Skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
-        }
-    });
-} )( jQuery );
 
 
 
