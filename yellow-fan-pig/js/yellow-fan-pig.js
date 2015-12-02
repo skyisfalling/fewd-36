e271dbcff74d6e9adce3f370f46464ac96708895
skyisfalling <skyisfalling@hotmail.com>
2015-12-02 00:59:46 -0800

    changed pig logos

--- yellow-fan-pig/js/yellow-fan-pig.js
+++ yellow-fan-pig/js/yellow-fan-pig.js
@@ -177,6 +177,18 @@ function createComment(comment) {
 // Utility functions
 // ------------------------------------------------
 
+//Skrollr
+
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
+
 
 
 
